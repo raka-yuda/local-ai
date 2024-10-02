e4c78ad54ceb2843590801ea8993cc0d1387a954
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-10-02 18:13:12 +0700

    feat: remove chakra ui from components

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import { Box, Button, HStack, Text, useClipboard } from "@chakra-ui/react";
-
-// function CodeBlock({ code, language }) {
-//   const { hasCopied, onCopy } = useClipboard(code);
-
-//   return (
-//     <Box borderWidth={1} borderRadius="md" overflow="hidden" my={4}>
-//       <HStack bg="gray.100" p={2} justifyContent="space-between">
-//         <Text fontSize="sm">{language}</Text>
-//         <Button onClick={onCopy} size="sm">
-//           {hasCopied ? 'Copied!' : 'Copy'}
-//         </Button>
-//       </HStack>
-//       <Box p={4} bg="gray.50" overflowX="auto">
-//         <pre style={{ margin: 0 }}>
-//           <code style={{ whiteSpace: 'pre', wordBreak: 'keep-all', overflowWrap: 'normal' }}>
-//             {code}
-//           </code>
-//         </pre>
-//       </Box>
-//     </Box>
-//   );
-// }
-
-// export default CodeBlock;
-
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
